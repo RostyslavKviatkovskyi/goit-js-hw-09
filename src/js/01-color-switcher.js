a22e5cedb4dfe7f885e669be79fcbb2c703034fc
Rostyslav Kviatkovskyi <allencor1234@gmail.com>
2022-07-31 22:14:37 +0300

    task_1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+bodyEl = document.querySelector('body');
+buttonStart = document.querySelector('button[data-start]');
+buttonStop = document.querySelector('button[data-stop]');
 
+buttonStart.addEventListener('click', onStartButtonClick);
+buttonStop.addEventListener('click', onStopButtonClick);
+
+let startInterval = null;
+
+function onStartButtonClick() {
+  buttonStart.disabled = true;
+  startInterval = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopButtonClick() {
+  buttonStart.disabled = false;
+  clearInterval(startInterval);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
