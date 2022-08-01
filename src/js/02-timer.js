aa511700e3d23794af6925e1dfa0deb9199cfb2d
Rostyslav Kviatkovskyi <allencor1234@gmail.com>
2022-08-01 16:48:12 +0300

    task_2_updated

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,22 +1,74 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
+let isButtonStartActive = false;
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
+  onClose(selectedDates, dateStr, instance) {
+    // console.log(selectedDates[0]);
+    selectedDate = selectedDates[0];
+  },
+  onChange(selectedDates) {
+    const DifferenceTime = selectedDates[0].getTime() - Date.now();
+    if (DifferenceTime > 0 && !isButtonStartActive) {
+      btnStartEl.setAttribute('disabled', '');
+      checkActiveStartButton();
+    }
+    if (DifferenceTime <= 0) {
+      window.alert('Please choose a date in the future');
+      if (isButtonStartActive) {
+        btnStartEl.removeAttribute('disabled', 'true');
+        checkActiveStartButton();
+      }
+    }
   },
 };
 
-btnStartEl = document.querySelector('button[data-start]');
-textInputEL = document.querySelector('#datetime-picker');
-spanDays = document.querySelector('span[data-days]');
-spanHours = document.querySelector('span[data-hours]');
-spanMinutes = document.querySelector('span[data-minutes]');
-spanSeconds = document.querySelector('span[data-seconds]');
+function checkActiveStartButton() {
+  if (isButtonStartActive) {
+    btnStartEl.setAttribute('disabled');
+    return;
+  } else {
+    btnStartEl.removeAttribute('disabled', 'true');
+  }
+}
+
+const btnStartEl = document.querySelector('button[data-start]');
+const timerOutput = document.querySelector('.timer');
+const textInputEL = document.querySelector('#datetime-picker');
+
+btnStartEl.addEventListener('click', onBtnStart);
+
+flatpickr('#datetime-picker', options);
+
+function onBtnStart() {
+  startTimer();
+  btnStartEl.setAttribute('disabled', 'false');
+}
+
+function startTimer() {
+  textInputEL.setAttribute('disabled', 'true');
+  const selectedDate = new Date(textInputEL.value);
+  const selectedTimeMs = selectedDate.getTime();
+  const intervalId = setInterval(() => {
+    const deltaTime = selectedTimeMs - Date.now();
+
+    if (deltaTime < 0 && deltaTime > -1000) {
+      clearInterval(intervalId);
+      textInputEL.removeAttribute('disabled', 'true');
+    } else {
+      updateTime(convertMs(deltaTime));
+    }
+  }, 1000);
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
 
 function convertMs(ms) {
   const second = 1000;
@@ -29,5 +81,17 @@ function convertMs(ms) {
   const minutes = Math.floor(((ms % day) % hour) / minute);
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
-  return { days, hours, minutes, seconds };
+  const getTime = { days, hours, minutes, seconds };
+  return getTime;
+}
+
+function updateTime(dateObj) {
+  const dateArray = Object.entries(dateObj);
+  dateArray.forEach(([dataSelector, value], index) => {
+    timerOutput.querySelector(`[data-${dataSelector}]`).textContent =
+      addLeadingZero(value);
+    if (dataSelector === 'days') {
+      return;
+    }
+  });
 }
